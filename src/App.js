03f331b35d3ba6667cb367ce137a2c690aec4732
Rohan <rrgstack@gmail.com>
2025-06-30 19:48:45 +0530

    router update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import "./styles.css";
 import "./search.css";
-import React, { Component } from "react";
+import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import about from "./pages/about";
-import works from "./pages/works";
+import About from "./pages/about";
+import Works from "./pages/works";
 import Home from "./pages/home";
-import writing from "./pages/writing";
-import social from "./pages/social";
+import Writing from "./pages/writing";
+import Social from "./pages/social";
 import AllResults from "./pages/allResults";
 import NotFound from "./pages/notFound";
 import ProjectsPage from "./pages/projectsPage";
 import ImagesPage from "./pages/imagesPage";
 import BlogPage from "./pages/blogPage";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/all" component={AllResults} />
-          <Route path="/about" component={about} />
-          <Route path="/writing" component={writing} />
-          <Route path="/works" component={works} />
-          <Route path="/social" component={social} />
-          <Route path="/projects" component={ProjectsPage} />
-          <Route path="/images" component={ImagesPage} />
-          <Route path="/blog" component={BlogPage} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/all" element={<AllResults />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/writing" element={<Writing />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="/social" element={<Social />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/images" element={<ImagesPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
